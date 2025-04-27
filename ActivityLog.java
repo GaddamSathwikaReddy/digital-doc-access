3abe62010c49f0dd11849e2d4c32e68b9d8ad8b6
GaddamSathwikaReddy <137271429+GaddamSathwikaReddy@users.noreply.github.com>
2025-04-28 00:35:49 +0300

    Update ActivityLog.java

--- ActivityLog.java
+++ ActivityLog.java
@@ -3,10 +3,23 @@ import java.sql.Timestamp;
 
 public class ActivityLog {
 
-	private int logID;
-	private int userID;
-	private int documentID;
-	private String action;
-	private Timestamp timestamp;
+    private int logID;
+    private int userID;
+    private int documentID;
+    private String action;
+    private Timestamp timestamp;
 
+    public ActivityLog() {}
+
+    public ActivityLog(int logID, int userID, int documentID, String action, Timestamp timestamp) {
+        this.logID = logID;
+        this.userID = userID;
+        this.documentID = documentID;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
+    public String getAction() {
+        return action;
+    }
 }
