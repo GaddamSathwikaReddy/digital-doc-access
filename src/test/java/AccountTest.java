988dfeea29f2b66b7bc2aa2cfb159846c52835e5
GaddamSathwikaReddy <137271429+GaddamSathwikaReddy@users.noreply.github.com>
2025-04-28 01:36:39 +0300

    Update AccountTest.java

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
+    
     @Test
     void testAccountNumberAssignment() {
         Account account = new Account();
