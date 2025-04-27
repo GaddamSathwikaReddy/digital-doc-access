28a6217afef66d91a84ddaf0fd575e41667200a0
GaddamSathwikaReddy <137271429+GaddamSathwikaReddy@users.noreply.github.com>
2025-04-28 00:56:10 +0300

    Update AccountTest.java

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,14 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-class AccountTest {
+public class AccountTest {
 
     @Test
-    void testAccountCreation() {
-        Account account = new Account("123456", 1, 30, "Email");
-        assertEquals("123456", account.getAccountNumber());
-        assertEquals(1, account.getUserID());
-        assertEquals(30, account.getDeliveryFrequency());
-        assertEquals("Email", account.getDeliveryMode());
+    void testAccountNumberAssignment() {
+        Account account = new Account();
+        account.accountNumber = "ACC12345";
+        assertEquals("ACC12345", account.accountNumber);
     }
 }
