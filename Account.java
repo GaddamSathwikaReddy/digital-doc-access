d02693c99de64d5c2823e92ffec8add98e0ffbbc
GaddamSathwikaReddy <137271429+GaddamSathwikaReddy@users.noreply.github.com>
2025-04-28 01:17:34 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,32 +1,15 @@
 public class Account {
-
     private String accountNumber;
     private int userID;
     private int deliveryFrequency;
     private String deliveryMode;
 
-    public Account() {}
-
-    public Account(String accountNumber, int userID, int deliveryFrequency, String deliveryMode) {
-        this.accountNumber = accountNumber;
-        this.userID = userID;
-        this.deliveryFrequency = deliveryFrequency;
-        this.deliveryMode = deliveryMode;
-    }
-
+    // Getter and setter for accountNumber
     public String getAccountNumber() {
         return accountNumber;
     }
 
-    public int getUserID() {
-        return userID;
-    }
-
-    public int getDeliveryFrequency() {
-        return deliveryFrequency;
-    }
-
-    public String getDeliveryMode() {
-        return deliveryMode;
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
     }
 }
